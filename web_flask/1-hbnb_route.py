a11928d21be32e681e3f8146b3e6b31804936f54
memoe <memoemukoe@gmail.com>
2023-12-27 03:23:08 -0800

    1. HBNB

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""web application must be listening on 0.0.0.0, port 5000
+Routes:
+/: display “Hello HBNB!”
+/hbnb: display HBNB """
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """  displays 'Hello HBNB!' """
+    return "Hello HBNB!"
+
+
+def hello_hbnb():
+    """displays HBNB"""
+    return "HBNB"
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
