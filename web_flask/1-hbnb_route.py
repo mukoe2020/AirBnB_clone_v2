db8dbc3a0ba80ca37949d5d9c64b0210e5f8f25f
memoemukoe <122824378+mukoe2020@users.noreply.github.com>
2023-12-27 13:33:41 +0200

    Update 1-hbnb_route.py

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -8,12 +8,12 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/hbnb", strict_slashes=False)
+@app.route("/", strict_slashes=False)
 def hello():
     """  displays 'Hello HBNB!' """
     return "Hello HBNB!"
 
-
+@app.route("/hbnb", strict_slashes=False)
 def hello_hbnb():
     """displays HBNB"""
     return "HBNB"
