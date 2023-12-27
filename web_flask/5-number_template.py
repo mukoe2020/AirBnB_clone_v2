4144df839ca4aa8469ffb74d3a010ef98942cba1
memoemukoe <122824378+mukoe2020@users.noreply.github.com>
2023-12-27 17:15:52 +0200

    Update 5-number_template.py

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,30 +1,33 @@
 #!/usr/bin/python3
-"""web application must be listening on 0.0.0.0, port 5000
+"""
+Starts a flask application
+This web application is listening on 0.0.0.0, port 5000
 Routes:
 /: display “Hello HBNB!”
-/hbnb: display HBNB
+/hbnb: display “HBNB”
 /c/<text>: display “C ” followed by the value of the text variable
+replacing all underscore with a space
 /python/<text>: display “Python ”, followed by the value of the text variable
-(replace underscore _ symbols with a space )
-The default value of text is “is cool”
-/number/<n>: display “n is a number” only if n is an integer
-/number_template/<n>: display a HTML page only if n is an integer:
-H1 tag: “Number: n” inside the tag BODY
-"""
+replacing all underscore with a space
+/number/<n>: displays 'n is a number' only if n is an integer
+/number_template/<n>: display a HTML page only if n is an integer
 
+"""
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
-def hello():
-    """  displays 'Hello HBNB!' """
+def hello_h():
+    """ Method that displays 'Hello HBNB!' """
     return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hello_hbnb():
-    """displays HBNB"""
+    """ Method that displays 'HBNB' """
     return "HBNB"
 
 
@@ -37,21 +40,21 @@ def display_text(text):
 
 @app.route("/python", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
-def display_python_text(text="is cool"):
-    """displays python followed by text value"""
+def display_p_text(text="is cool"):
+    """Displays 'Python' and then the value of <text> """
     text = text.replace("_", " ")
     return "Python {}".format(text)
 
 
 @app.route("/number/<int:n>", strict_slashes=False)
-def number_display(n):
-    """ display “n is a number” only if n is an integer"""
-    return f"{n} is a number"
+def display_number(n):
+    """Method that displays 'n' only if it is an integer"""
+    return "{} is a number".format(n)
 
 
 @app.route("/number_template/<int:n>", strict_slashes=False)
 def display_num_template(n):
-    """displays body of a template if n is an integer"""
+    """Method that displays body of a template if n is an integer"""
     return render_template("5-number.html", n=n)
 
 
