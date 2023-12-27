ff871cecf844880d87c30d4e983833f2b7569aee
memoe <memoemukoe@gmail.com>
2023-12-27 03:03:29 -0800

    0. Hello Flask!

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" a flask application
+This web application is listening on 0.0.0.0, port 5000
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """  displays 'Hello HBNB!' """
+    return "Hello HBNB!"
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
